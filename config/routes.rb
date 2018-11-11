4f11813ad9971930f0e9bec075fe0df522cb56e0
team-zaki <zaki.ismail@gmail.com>
2018-11-11 03:09:17 +0000

    17. Sections

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show]
   # Connect instructor controller in config/routes.rb to 'instructor namespace' course controller
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
 end
