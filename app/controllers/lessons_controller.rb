60166f618861992234a9aa593429478ed4b5b1f5
team-zaki <zaki.ismail@gmail.com>
2018-11-14 15:52:14 +0000

    Permissions updates

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,20 @@
 class LessonsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
 
+  def require_authorized_for_current_course
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to courses_path(current_lesson.section.course), :alert => "You must be enrolled to view the lesson"
+    end
+  end
+
   helper_method :current_lesson
+  
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
