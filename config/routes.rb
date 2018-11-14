b6d4240c4fd22a3488c94b6adea5ff52bf7c4228
team-zaki <zaki.ismail@gmail.com>
2018-11-14 03:18:00 +0000

    28. Permissions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   # allows courses to be stored in database. Students need to view all the courses available on our web app
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   # Connect instructor controller in config/routes.rb to 'instructor namespace' course controller
   namespace :instructor do
