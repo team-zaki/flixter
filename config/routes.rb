f50c18f1e592a126c5b6883184b96c288d1b672d
team-zaki <zaki.ismail@gmail.com>
2018-11-23 21:31:30 +0000

    34. Instructor dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  # singleton route for dashboards controller
+  resource :dashboard, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   # allows courses to be stored in database. Students need to view all the courses available on our web app
